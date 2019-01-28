69315f7937f51b44fb52cb4dc8c5c3c4ee30d983
Rafael Costa <costarafael159@gmail.com>
2019-01-28 10:30:01 -0200

    change struct and methods names

--- data structures/doubly_linked_list.c
+++ data structures/doubly_linked_list.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include <limits.h>
 
-struct doubly_node {
+struct dnode {
     int key;
-    struct doubly_node * next;
-    struct doubly_node * previus;
+    struct dnode * next;
+    struct dnode * previus;
 };
 
-struct doubly_list {
-    struct doubly_node * head;
+struct dlist {
+    struct dnode * head;
 };
 
-struct doubly_node * create_dnode(int k){
-    struct doubly_node * n = malloc(sizeof(struct doubly_node));
+struct dnode * create_dnode(int k){
+    struct dnode * n = malloc(sizeof(struct dnode));
     n->key = k;
     n->next = NULL;
     n->previus = NULL;
     return n;
 }
 
-struct doubly_list * create_dlist(){
-    struct doubly_list * d = malloc(sizeof(struct doubly_list));
+struct dlist * create_dlist(){
+    struct dlist * d = malloc(sizeof(struct dlist));
     return d;
 }
 
-void print_dlist(struct doubly_list * d){
-    struct doubly_node * a = d->head;
+void print_dlist(struct dlist * d){
+    struct dnode * a = d->head;
     while(a != NULL){
         printf("%d, ", a->key);
         a = a->next;
@@ -34,18 +34,18 @@ void print_dlist(struct doubly_list * d){
     printf("\n");
 }
 
-int dlist_empty(struct doubly_list * d){
+int dlist_empty(struct dlist * d){
     return d->head == NULL;
 }
 
-struct doubly_node * dlist_search(struct doubly_list * d, int k){
-    struct doubly_node * x = d->head;
+struct dnode * dlist_search(struct dlist * d, int k){
+    struct dnode * x = d->head;
     while(x != NULL && x->key != k)
         x = x->next;
     return x;
 }
 
-void _dlist_insert(struct doubly_list * d, struct doubly_node * x){
+void _dlist_insert(struct dlist * d, struct dnode * x){
     x->next = d->head;
     if(!dlist_empty(d))
         d->head->previus = x;
@@ -53,12 +53,12 @@ void _dlist_insert(struct doubly_list * d, struct doubly_node * x){
     x->previus = NULL;
 }
 
-void dlist_insert(struct doubly_list * d, int k){
-    struct doubly_node * x = create_dnode(k);
+void dlist_insert(struct dlist * d, int k){
+    struct dnode * x = create_dnode(k);
     _dlist_insert(d, x);
 }
 
-void _dlist_delete(struct doubly_list * d, struct doubly_node * x){
+void _dlist_delete(struct dlist * d, struct dnode * x){
     if(x->previus != NULL)
         x->previus->next = x->next;
     else
@@ -68,8 +68,8 @@ void _dlist_delete(struct doubly_list * d, struct doubly_node * x){
     free(x);
 }
 
-int dlist_delete(struct doubly_list * d, int x){
-    struct doubly_node * n = dlist_search(d, x);
+int dlist_delete(struct dlist * d, int x){
+    struct dnode * n = dlist_search(d, x);
     if(n != NULL){
         _dlist_delete(d, n);
         return 1;
@@ -78,9 +78,9 @@ int dlist_delete(struct doubly_list * d, int x){
         return 0;
 }
 
-void destroy_dlist(struct doubly_list * d){
-    struct doubly_node * a = d->head;
-    struct doubly_node * p;
+void destroy_dlist(struct dlist * d){
+    struct dnode * a = d->head;
+    struct dnode * p;
     while(a != NULL){
         p = a;
         a = a->next;
